5a4bd24533022502e3432c71deb7197066d82843
antoinestevens <antoine.stevens@uclouvain.be>
2013-08-14 09:59:18 +0200

    doc fix (less than 100 char)

--- R/gapDer.r
+++ R/gapDer.r
@@ -20,16 +20,20 @@
 #' opar <- par(no.readonly = TRUE)
 #' par(mfrow=c(2,2),mar=c(4,4,2,2))
 #' # plot of the 10 first spectra
-#' matplot(as.numeric(colnames(spc)),t(spc[1:10,]),type='l',xlab='',ylab='Reflectance') 
+#' matplot(as.numeric(colnames(spc)),t(spc[1:10,]),
+#'         type='l',xlab='',ylab='Reflectance') 
 #' mtext('Raw spectra')
 #' der <- gapDer(spc,m=1,w=1,s = 1,delta.wav=2)
-#' matplot(as.numeric(colnames(der)),t(der[1:10,]),type='l',xlab='Wavelength /nm',ylab='gap derivative') 
+#' matplot(as.numeric(colnames(der)),t(der[1:10,]),
+#'         type='l',xlab='Wavelength /nm',ylab='gap derivative') 
 #' mtext('1st derivative spectra')
 #' der <- gapDer(spc,m=1,w=11,s = 1,delta.wav=2)
-#' matplot(as.numeric(colnames(der)),t(der[1:10,]),type='l',xlab='Wavelength /nm',ylab='gap derivative') 
+#' matplot(as.numeric(colnames(der)),t(der[1:10,]),
+#'         type='l',xlab='Wavelength /nm',ylab='gap derivative') 
 #' mtext('1st derivative spectra with a window size = 11 nm')
 #' der <- gapDer(spc,m=1,w=11,s = 10,delta.wav=2)
-#' matplot(as.numeric(colnames(der)),t(der[1:10,]),type='l',xlab='Wavelength /nm',ylab='gap derivative') 
+#' matplot(as.numeric(colnames(der)),t(der[1:10,]),
+#'         type='l',xlab='Wavelength /nm',ylab='gap derivative') 
 #' mtext('1st derivative spectra with a window size = 11 nm, smoothing of 10 nm')
 #' par(opar)
 
