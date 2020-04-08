e40fb5921ffaeaea9798d6d127874015abb6fb2c
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2020-04-08 12:18:01 +0200

    seville v

--- R/AAA.R
+++ R/AAA.R
@@ -3,7 +3,7 @@
 .onAttach <- function(lib, pkg) {
     #assign("gpclib", FALSE, envir=.prospectr_CACHE)
     prospectr.v <- read.dcf(file = system.file("DESCRIPTION", package=pkg), fields="Version")
-    packageStartupMessage(paste(pkg, "version", prospectr.v, "-- 'waving'"))
+    packageStartupMessage(paste(pkg, "version", prospectr.v, "-- 'seville'"))
     packageStartupMessage("check the github repository at http://github.com/l-ramirez-lopez/prospectr")
 }
 
