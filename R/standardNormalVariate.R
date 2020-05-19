72e02a5b83331f4325d3953f46f2e796b7235227
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2020-05-19 14:30:38 +0200

    example formatting

--- R/standardNormalVariate.R
+++ R/standardNormalVariate.R
@@ -12,11 +12,13 @@
 #' spc <- 1 / 10^NIRsoil$spc # conversion to reflectance
 #' snv <- standardNormalVariate(X = spc)
 #' # 10 first snv spectra
-#' matplot(x = as.numeric(colnames(snv)),
-#'         y = t(snv[1:10, ]), 
-#'         type = "l", 
-#'         xlab = "wavelength, nm", 
-#'         ylab = "snv")
+#' matplot(
+#'   x = as.numeric(colnames(snv)),
+#'   y = t(snv[1:10, ]),
+#'   type = "l",
+#'   xlab = "wavelength, nm",
+#'   ylab = "snv"
+#' )
 #' \dontrun{
 #' apply(snv, 1, sd) # check
 #' }
