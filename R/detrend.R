2e564d89250a10d8b70a399a5701232043f95ee9
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2020-09-23 00:48:58 +0200

    pol order argumnet

--- R/detrend.R
+++ R/detrend.R
@@ -1,48 +1,61 @@
-#' @title Detrend transformation
+#' @title Detrending spectral data
 #' @description
 #' \lifecycle{stable}
+#' \loadmathjax
 #' Normalizes each row of an input matrix by applying a SNV transformation
-#' followed by fitting a second order linear model and returning the fitted residuals.
+#' followed by fitting a second order linear model and returning the fitted 
+#' residuals.
 #' @usage
-#' detrend(X, wav)
-#' @param X a numeric matrix or vector to process  (optionally a data frame that can
-#' be coerced to a numerical matrix)
+#' detrend(X, wav, p = 2)
+#' @param X a numeric matrix or vector to process  (optionally a data frame that 
+#' can be coerced to a numerical matrix)
 #' @param wav the wavelengths/ band centers.
-#' @author Antoine Stevens
+#' @param p an integer larger than 1 indicating the polynomial order (default is 
+#' 2, as in the original paper of Barnes et al., 1989).
+#' @author Antoine Stevens and Leonardo Ramirez-Lopez
 #' @examples
 #' data(NIRsoil)
 #' wav <- as.numeric(colnames(NIRsoil$spc))
 #' # conversion to reflectance
-#' spc <- 1 / 10^NIRsoil$spc
 #' opar <- par(no.readonly = TRUE)
 #' par(mfrow = c(2, 1), mar = c(4, 4, 2, 2))
 #' # plot of the 10 first spectra
-#' matplot(wav, t(spc[1:10, ]),
+#' matplot(wav, t(NIRsoil$spc[1:10, ]),
 #'   type = "l",
 #'   xlab = "",
-#'   ylab = "Reflectance"
+#'   ylab = "Absorbance"
 #' )
 #' mtext("Raw spectra")
-#' det <- detrend(spc, wav)
+#' det <- detrend(NIRsoil$spc, wav)
 #' matplot(wav, t(det[1:10, ]),
 #'   type = "l",
 #'   xlab = "Wavelength /nm",
-#'   ylab = "Reflectance"
+#'   ylab = "Absorbance"
 #' )
 #' mtext("Detrend spectra")
 #' par(opar)
-#' @details The detrend is a row-wise transformation that allows to correct for wavelength-dependent
-#' scattering effects (variations in curvilinearity). A second-degree polynomial is fit through each spectrum:
-#' \deqn{x_i = a\lambda^2 + b\lambda + c + e_i}
-#' were \eqn{x_i} is the spectrum, \eqn{\lambda} is the wavelength vector, a, b, c are estimated by least square,
-#' and \eqn{e_i} are the residuals of the least square fit. Then, a detrend spectrum corresponds to:
-#' \deqn{x\ast_i = x_i - (a\lambda^2 + b\lambda + c = e_i)}
-#' @seealso \code{\link{standardNormalVariate}}, \code{\link{blockScale}}, \code{\link{blockNorm}}
-#' @references Barnes RJ, Dhanoa MS, Lister SJ. 1989. Standard normal variate transformation and de-trending of near-infrared diffuse reflectance spectra. Applied spectroscopy, 43(5): 772-777.
-#' @return a matrix or vector with detrend values
+#' @details The detrend is a row-wise transformation that allows to correct for 
+#' wavelength-dependent scattering effects (variations in curvilinearity). A 
+#' \mjeqn{p}{p} order polynomial is fit for each spectrum (\mjeqn{x_i}{x_i}) 
+#' using the vector of bands (\mjeqn{\lambda}{\lambda}, e.g. wavelengths) as 
+#' explanatory variable as follows:
+#' 
+#' \mjdeqn{x_i = a\lambda^p + ... + b\lambda + c + e_i}{x_i = a\lambda^p + ... + b\lambda + c + e_i}
+#' 
+#' were a, b, c are estimated by least squares, and \mjeqn{e_i}{e_i} are the 
+#' spectral residuals of the least square fit. The residuals of the \mjeqn{i}{i}th 
+#' correspond to the \mjeqn{i}{i}th detrended spectrum.  
+#' 
+#' @seealso \code{\link{standardNormalVariate}}, \code{\link{blockScale}},
+#'  \code{\link{blockNorm}}
+#' @references Barnes RJ, Dhanoa MS, Lister SJ. 1989. Standard normal variate 
+#' transformation and de-trending of near-infrared diffuse reflectance spectra. 
+#' Applied spectroscopy, 43(5): 772-777.
+#' @return a matrix or vector with the detrended data.
 #' @export
-#'
-detrend <- function(X, wav) {
+
+detrend <- function(X, wav, p = 2) {
+  
   if (missing(wav)) {
     stop("argument wav should be specified")
   }
@@ -51,22 +64,29 @@ detrend <- function(X, wav) {
     X <- as.matrix(X)
   }
 
-  was.vec <- is.vector(X)
-
+  was_vec <- is.vector(X)
+  if (p < 1) {
+    stop("'p' must be an integer larger than 0")
+  }
+  
+  if (p != round(p)) {
+    stop("'p' must be an integer")
+  }
+  
   if (is.vector(X)) {
     nms <- names(X)
     X <- matrix(X, ncol = length(X))
   }
 
-  xpoly <- stats::poly(wav, 2)
+  xpoly <- stats::poly(wav, p)
   # SNV transformation
   X <- sweep(X, 1, rowMeans(X), "-")
   X <- sweep(X, 1, apply(X, 1, sd), "/")
 
   # get the residuals output <- t(apply(X, 1, function(y) lm.fit(x= xpoly,y)$residuals))
   output <- residLm(X, xpoly) # using Rcpp ...
-
-  if (was.vec) {
+  
+  if (was_vec) {
     output <- as.vector(output)
     names(output) <- nms
   } else {
--- src/residLm.cpp
+++ src/residLm.cpp
@@ -7,12 +7,12 @@ NumericMatrix residLm(NumericMatrix Yr, NumericMatrix Xr) {
    
    int nX = Xr.nrow(), nY = Yr.nrow();
    arma::mat Y(Yr.begin(), nY, nX, false); // reuses memory and avoids extra copy
-   arma::mat X(Xr.begin(), nX, 2, false);
-   arma::mat resid(nX,nY);
+   arma::mat X(Xr.begin(), nX, Xr.ncol(), false);
+   arma::mat resid(nX, nY);
    arma::colvec y;
    for(int i = 0; i < nY; i++){     
      y = Y.row(i).t();
-     resid.col(i) = y - X*arma::solve(X, y);    
+     resid.col(i) = y - X * arma::solve(X, y);    
   }
      
    return wrap(resid.t());
