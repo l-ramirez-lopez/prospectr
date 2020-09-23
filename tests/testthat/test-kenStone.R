7d377ef5228c724146e19a5e591882d1927373d1
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2020-09-23 11:38:24 +0200

    Update test-kenStone.R

--- tests/testthat/test-kenStone.R
+++ tests/testthat/test-kenStone.R
@@ -3,13 +3,11 @@ context("test-kenStone")
 test_that("kenStone works", {
   nirdata <- data("NIRsoil")
   
-  X_kenStone <- kenStone(NIRsoil$spc, k = 30)
+  X_kenStone <- kenStone(NIRsoil$spc, k = 5, metric = "mahal", pc = 3)
   X_kenStone$model
   
   expect_is(X_kenStone, "list")
-  
-  sel_samples <- c(619, 402, 141, 614, 186, 715, 618, 248, 710, 410, 823, 147, 
-                   818, 686, 706, 617, 612, 290, 572, 825, 615, 284, 320, 578, 
-                   313, 279, 282, 351, 540, 204)
+
+  sel_samples <- c(687, 377, 410, 619,  87)
   expect_true(!any(!sel_samples == X_kenStone$model))
 })
