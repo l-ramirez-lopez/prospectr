32590b1c1073cdf80bc7768c151b42a8dfa02328
Leo <ramirez.lopez.leo@gmail.com>
2021-11-26 18:08:13 +0100

    fix(no stripping of symbols)

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // bitAND
 int bitAND(int aa, int bb);
 RcppExport SEXP _prospectr_bitAND(SEXP aaSEXP, SEXP bbSEXP) {
