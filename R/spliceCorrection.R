e71feee0457220b9ead6518069324183c256a2b5
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2022-02-17 21:37:59 +0100

    tweak(stlyer)

--- R/spliceCorrection.R
+++ R/spliceCorrection.R
@@ -7,52 +7,51 @@
 #' @param X a numeric matrix or vector to transform (optionally a data frame that can
 #' be coerced to a numerical matrix).
 #' @param wav a numeric vector with band positions.
-#' @param splice a numeric vector of length 1 or 2 with the positions of the 
-#' splice(s). Default: 
-#' \code{c(1000, 1830)} (as for the ASD FieldSpec Pro spectrometer of Malvern 
+#' @param splice a numeric vector of length 1 or 2 with the positions of the
+#' splice(s). Default:
+#' \code{c(1000, 1830)} (as for the ASD FieldSpec Pro spectrometer of Malvern
 #' Panalytical). See details.
 #' @param interpol.bands the number of interpolation bands.
 #' @details
-#' This function uses by default the positions for the ASD FieldSpec Pro 
+#' This function uses by default the positions for the ASD FieldSpec Pro
 #' spectroradiometer (Malvern Panalytical) which usually exhibit
 #' steps at the splice of the three built-in detectors,
 #' positioned at 1000 nm (end of VNIR detector) and 1830 nm (end of SWIR1 detector).
-#' The data corresponding to the spectral region after the first step is used as 
-#' reference for correcting the first region and the laste region (if 2 steps 
+#' The data corresponding to the spectral region after the first step is used as
+#' reference for correcting the first region and the laste region (if 2 steps
 #' are supplied).
-#' Other typical examples of splice artifacts caused by concatenating data 
+#' Other typical examples of splice artifacts caused by concatenating data
 #' captured by different detectors inside the spectrometer:
 #' \itemize{
 #'  \item{XDS (FOSS)}{ 1100 nm}
 #'  \item{ProxiMate (BUCHI Labortechnik)}{ 900 nm}
 #' }
-#' 
-#' 
+#'
+#'
 #' @return a matrix with the splice corrected data.
-#' @author Antoine Stevens and href{https://orcid.org/0000-0002-5369-5120}{Leonardo Ramirez-Lopez} 
+#' @author Antoine Stevens and href{https://orcid.org/0000-0002-5369-5120}{Leonardo Ramirez-Lopez}
 #' @export
 
 spliceCorrection <- function(X, wav, splice = c(1000, 1830), interpol.bands = 10) {
-  
   extrapfun <- function(x, y, xout) {
     fit <- lm(y ~ x)
     fit$coefficients[1] + fit$coefficients[2] * xout
   }
-  
-  if (length(splice) <  1 | length(splice) > 2) {
+
+  if (length(splice) < 1 | length(splice) > 2) {
     stop("splice must be a numeric vector of length 1 or 2")
   }
-  
+
   if (length(splice) == 1) {
     index_b <- ncol(X)
   }
-  
+
   has_three_regions <- length(splice) == 2
-  
+
   if (is.data.frame(X)) {
     X <- as.matrix(X)
   }
-  
+
   was_vec <- is.vector(X)
   if (is.vector(X)) {
     nms <- names(X)
@@ -61,25 +60,25 @@ spliceCorrection <- function(X, wav, splice = c(1000, 1830), interpol.bands = 10
   if (missing(wav)) {
     wav <- seq_len(ncol(X))
   }
-  
+
   if (length(wav) != ncol(X)) {
     stop("length(wav) must be equal to ncol(X)")
   }
-  
+
   index <- which(wav %in% splice)
-  
+
   if (!length(index)) {
     stop("splice positions not found in wav")
   }
-  
+
   Xa <- X[, 1:index[1], drop = FALSE]
   Xb <- X[, (index[1] + 1):index_b, drop = FALSE]
-  
+
   tmp_first <- Xb[, 1:interpol.bands, drop = FALSE]
   w_first <- wav[(index[1] + 1):(index[1] + interpol.bands)]
   pred_Xa <- apply(tmp_first, 1, function(y) extrapfun(x = w_first, y = y, xout = splice[1]))
   offset_a <- Xa[, ncol(Xa)] - pred_Xa
-  
+
   if (has_three_regions) {
     Xc <- X[, (index[2] + 1):ncol(X), drop = FALSE]
     tmp_second <- Xb[, (ncol(Xb) - interpol.bands + 1):ncol(Xb), drop = FALSE]
