c79dede7981d034f7cbe8b614d26d5b3dd3861e2
Leo <ramirez.lopez.leo@gmail.com>
2022-07-18 11:41:17 +0200

    style(replace F by FALSE)

--- R/kenstone.R
+++ R/kenstone.R
@@ -127,7 +127,7 @@ kenStone <- function(X,
         pc <- 1
       }
     }
-    scores <- X <- pca$x[, 1:pc, drop = F]
+    scores <- X <- pca$x[, 1:pc, drop = FALSE]
   }
 
   if (metric == "mahal") {
