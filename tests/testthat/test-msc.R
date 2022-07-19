02ab4d464634cad9cee37489017c8f5c65840b43
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2022-07-19 11:45:33 +0200

    style(styler)

--- tests/testthat/test-msc.R
+++ tests/testthat/test-msc.R
@@ -8,7 +8,7 @@ test_that("msc", {
   expect_is(X_msc, "matrix")
   expect_true(round(max(X_msc[1, ]), 5) == 0.37394)
   expect_true(round(min(X_msc[1, ]), 5) == 0.29474)
-  
+
   X_mscb <- msc(NIRsoil$spc, apply(NIRsoil$spc, 2, median))
   expect_true(round(max(X_mscb[1, ]), 5) == 0.34816)
   expect_true(round(min(X_mscb[1, ]), 5) == 0.26749)
