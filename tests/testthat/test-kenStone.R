cd902a1fb9a991989053f583f3de5513b9acb698
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2022-07-19 11:45:29 +0200

    style(styler)

--- tests/testthat/test-kenStone.R
+++ tests/testthat/test-kenStone.R
@@ -8,21 +8,23 @@ test_that("kenStone works", {
 
   expect_is(X_kenStone, "list")
 
-  sel_samples <- c(687, 377, 410, 619, 87, 618, 611, 283, 317, 666, 789, 635,
-                   147, 822, 285, 313, 737, 803, 819, 383, 823, 204, 591, 252, 
-                   825, 272, 402, 39, 330, 590, 286, 608, 363, 234, 701, 718,
-                   287, 270, 571, 192, 614, 1, 386, 615, 126, 755, 734, 428,
-                   466, 426)
+  sel_samples <- c(
+    687, 377, 410, 619, 87, 618, 611, 283, 317, 666, 789, 635,
+    147, 822, 285, 313, 737, 803, 819, 383, 823, 204, 591, 252,
+    825, 272, 402, 39, 330, 590, 286, 608, 363, 234, 701, 718,
+    287, 270, 571, 192, 614, 1, 386, 615, 126, 755, 734, 428,
+    466, 426
+  )
   expect_true(!any(!sel_samples == X_kenStone$model))
-  
+
   X_kenStone_b <- kenStone(
     NIRsoil$spc,
     k = 50,
     metric = "mahal",
-    pc = 3, 
+    pc = 3,
     init = X_kenStone$model[1:10]
   )
-  
+
   expect_true(!any(!sel_samples == X_kenStone_b$model))
 })
 
@@ -40,11 +42,12 @@ test_that("kenStone works with groups", {
   if (length(my_groups) != nrow(x)) {
     my_groups <- c(rep(nrow(x), nrow(x) - length(my_groups)), my_groups)
   }
-  
-  X_kenStone <- kenStone(x, 
-                         k = 30, 
-                         pc = 2, 
-                         group = as.factor(my_groups))
-  
+
+  X_kenStone <- kenStone(x,
+    k = 30,
+    pc = 2,
+    group = as.factor(my_groups)
+  )
+
   expect_true(all(diff(X_kenStone$model[1:n_per_group]) == 1))
 })
