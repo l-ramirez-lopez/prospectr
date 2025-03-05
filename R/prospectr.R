dd481477ddaae227ec5d3caf05a430c6d133b23f
l-ramirez-lopez <leonardo.ramirez-lopez23@imperial.ac.uk>
2025-03-06 00:28:46 +0100

    doc: update copyright year

--- R/prospectr.R
+++ R/prospectr.R
@@ -26,7 +26,7 @@
 #'
 #' @details
 #' 
-#' #' This is the version 
+#' This is the version 
 #' `r paste(pkg_info()[1:2], collapse = " \U002D\U002D ")` of the package. 
 #' The main functionality is listed here. 
 #'
@@ -89,7 +89,7 @@
 #' }
 ######################################################################
 # prospectr
-# Copyrigth (C) 2024 Leonardo Ramirez-Lopez
+# Copyrigth (C) 2025 Leonardo Ramirez-Lopez
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
